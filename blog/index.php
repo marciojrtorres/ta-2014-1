4cad03c8d0e3c04d203978bb9f0e13e925874c62
Márcio Torres <marciojrtorres@gmail.com>
2014-05-22 18:54:17 -0300

    alteracoes

--- blog/index.php
+++ blog/index.php
@@ -1,10 +1,3 @@
-<?php 
-require 'vendor/autoload.php';
-// conecta no servidor de cache
-Predis\Autoloader::register();
-$redis = new Predis\Client();
-?>
-
 <!DOCTYPE html>
 
 <html>
@@ -13,11 +6,6 @@ $redis = new Predis\Client();
     <title>UM BLOG</title>
     <link rel="stylesheet" type="text/css" href="estilo.css">
     <script type="text/javascript" src="jquery.js"></script>
-    <script type="text/javascript">
-    $(function() {
-        $('div#detalhes').load('detalhes');
-    });
-    </script>
 </head>
 <body>
     
@@ -28,22 +16,10 @@ $redis = new Predis\Client();
     <img src="logo.jpg" alt="logo do blog">
 
     <?php
-    // pega do cache
-    $conteudo = $redis->get('index');
-    // se encontrou no cache
-    if ($conteudo) {
-        echo "conteudo veio do cache";
-        // usa o cache (mostra)
-        echo $conteudo;
-    } else { // se não está no cache: banco
-        echo "conteudo veio do banco";
-
-        $link = mysql_connect('localhost', 'root', 'root') or die ('banco de dados indisponivel');
-        mysql_select_db('blog', $link) or die ('banco de dados blog nao existe');
-        $sql = "SELECT * FROM posts"; // LIMIT 2 OFFSET " . $offset;    
-        $posts = mysql_query($sql, $link);
-
-        ob_start();
+    $link = mysql_connect('localhost', 'root', 'root') or die ('banco de dados indisponivel');
+    mysql_select_db('blog', $link) or die ('banco de dados blog nao existe');
+    $sql = "SELECT * FROM posts"; // LIMIT 2 OFFSET " . $offset;    
+    $posts = mysql_query($sql, $link);
     ?>
 
     <? while ($post = mysql_fetch_assoc($posts)): ?>
@@ -55,17 +31,5 @@ $redis = new Predis\Client();
     </div>
     <? endwhile ?>
 
-    <?php 
-    $conteudo = ob_get_flush();
-    // coloca o conteudo no cache
-    $redis->set('index',$conteudo);
-    $redis->expire('index',10);
-
-    } 
-    ?>
-
-    <div id="detalhes">
-
-    </div>
 </body>
 </html>
