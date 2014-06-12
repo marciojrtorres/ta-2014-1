4f764d8a6c1dc9c925324c615049f4837dbfaf53
Márcio Torres <marciojrtorres@gmail.com>
2014-06-11 21:39:24 -0300

    blog3

--- blog3-resolvido/post.php
+++ blog3-resolvido/post.php
@@ -14,40 +14,58 @@ require 'vendor/autoload.php';
 <body>
 
     <?php
-
+        /*
         $link = mysql_connect('localhost', 'root', 'root') or die ('banco de dados indisponivel');
         mysql_select_db('blog', $link) or die ('banco de dados blog nao existe');
         $sql = "SELECT * FROM posts WHERE id = " . $_GET['id']; 
         $post = mysql_fetch_assoc(mysql_query($sql, $link));
-
+        */
+        $client = new couchClient ('http://127.0.0.1:5984','blog');
+        $post = $client->getDoc($_GET['id']);
+        
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $comentario = new stdClass();
+            $comentario->texto = $_REQUEST['comentario'];
+            $comentario->autor = $_REQUEST['autor'];
+            if (isset($post->comentarios)) {
+                $post->comentarios[] = $comentario;
+            } else {                
+                $post->comentarios = array($comentario);
+            }
+            $client->storeDoc($post);
+            /*
             $id = $_REQUEST['id'];
             $comentario = $_REQUEST['comentario'];
             mysql_query("INSERT INTO comentarios VALUES ($id, '$comentario')", $link);
+            */
         }
         
     ?>
 
-    <h2><?= $post['titulo'] ?></h2>
+    <h2><?= $post->titulo ?></h2>
 
-    <p><?= $post['texto'] ?></p>
+    <p><?= $post->texto ?></p>
 
     <form method="post">        
+        <label>Autor</label>
+        <input type="text" name="autor">
         <label>Comentário:</label>
         <textarea name="comentario"></textarea>
         <input type="submit">
     </form>
 
     <?php
+        /*
         $sql = "SELECT * FROM comentarios WHERE id_post = " . $_GET['id']; 
         $comentarios = mysql_query($sql, $link);
+        */
     ?>
 
     <a href="asdasd" rel="nofollow"></a>
 
-    <? while ($coment = mysql_fetch_assoc($comentarios)): ?>
-        <p><?= $coment['comentario'] ?></p>        
-    <? endwhile ?>
+    <? foreach ($post->comentarios as $comentario): // while ($coment = mysql_fetch_assoc($comentarios)): ?>
+        <p><?= $comentario->texto ?> por autor <?= isset($comentario->autor) ? $comentario->autor : 'desconhecido' ?></p>        
+    <? endforeach // endwhile ?>
 
 </body>
 </html>
